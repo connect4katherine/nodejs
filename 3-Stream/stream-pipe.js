b7592f90013ca5fde08811c8b34ef510fd54190f
krosenk729 <krosenk729@gmail.com>
2018-05-28 04:55:20 -0400

    simple stream ex

--- 3-Stream/stream-pipe.js
+++ 3-Stream/stream-pipe.js
@@ -1,8 +1,23 @@
-const fs = require("fs");
+const fs = require('fs');
+var zlib = require('zlib');
 
 // ============================================================
 // Create streams
 const readerStream = fs.createReadStream('input.txt');
+
+/*
+// Compress the file input.txt to input.txt.gz
+fs.createReadStream('input.txt')
+   .pipe(zlib.createGzip())
+   .pipe(fs.createWriteStream('input.txt.gz'));
+*/
+
+/*
+// Decompress the file input.txt.gz to input.txt
+fs.createReadStream('input.txt.gz')
+   .pipe(zlib.createGunzip())
+   .pipe(fs.createWriteStream('input.txt'));
+*/
 
 const writerStream = fs.createWriteStream('output.txt');
 
@@ -10,4 +25,5 @@ const writerStream = fs.createWriteStream('output.txt');
 // Pipe between
 
 readerStream.pipe(writerStream);
+
 console.log("Program Ended");
