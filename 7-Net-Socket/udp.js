5575692bd1a56e698e252cebe05f67058af0ae69
krosenk729 <krosenk729@gmail.com>
2018-05-28 06:21:46 -0400

    playing with udp

--- 7-Net-Socket/udp.js
+++ 7-Net-Socket/udp.js
@@ -1,20 +1,43 @@
 const dgram = require('dgram');
-const server = dgram.createSocket('udp4');
-
-// ============================================================
-// 
-
-server.on('listening', ()=>{
-	console.log('UDP listening...');
-});
-
-server.on('message', (msg, rinfo)=>{
-
-});
-
-// ============================================================
-// 
-
 const port = 3000;
 const host = '127.0.0.1';
+
+// ============================================================
+// https://nodejs.org/api/dgram.html
+// server-side
+
+const server = dgram.createSocket('udp4');
+
+server.on('listening', ()=>{
+	console.log('UDP Server listening...\n');
+});
+
+server.on('message', (msg, removed_info)=>{
+	console.log(`${removed_info.address}:${removed_info.port} - ${msg}`);
+	console.log('...UDP Server message\n');
+});
+
 server.bind(port, host);
+
+// ============================================================
+// client-side
+
+const client = dgram.createSocket('udp4');
+client.send('SMILE!', port, host, (err) =>{
+	if(err) throw err;
+
+	console.log('Client callback\n');
+	client.close();
+});
+
+/*
+setInterval(function() {
+	const client = dgram.createSocket('udp4');
+	client.send('SMILE!', port, host, (err) =>{
+		if(err) throw err;
+
+		console.log('Client callback\n');
+		client.close();
+	});
+},1000);
+*/
