139a67ec27609510bad686b2ca747bcb5f1ba4d9
krosenk729 <krosenk729@gmail.com>
2018-06-01 20:27:18 -0400

    messing with os

--- 9-Misc/os.js
+++ 9-Misc/os.js
@@ -34,3 +34,22 @@ ${cmd} :
 });
 
 
+/*
+os.EOL
+os.arch()
+os.constants
+os.cpus()
+os.endianness()
+os.freemem()
+os.homedir()
+os.hostname()
+os.loadavg()
+os.networkInterfaces()
+os.platform()
+os.release()
+os.tmpdir()
+os.totalmem()
+os.type()
+os.uptime()
+os.userInfo()
+*/
