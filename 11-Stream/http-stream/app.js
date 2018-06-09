12960730a7584a6bd5c4fc07ebd0d2e5a1853b75
krosenk729 <krosenk729@gmail.com>
2018-06-09 05:59:57 -0400

    adding mini uploader

--- 11-Stream/http-stream/app.js
+++ 11-Stream/http-stream/app.js
@@ -4,6 +4,8 @@
 const PORT = 3000;
 const Path = require('path');
 const FS = require('fs');
+const ejs = require('ejs');
+const db = require('mongodb');
 
 // ============================================================
 // 
@@ -18,21 +20,28 @@ server.on('request', (req, res)=>{
 	// console.log(Object.keys(req.readable));
 
 	switch(req.url){
+		case '/':
+		if(req.method === 'GET'){
+			// res.writeHead(200, {'content-type': 'text/html'});
+			// res.end(FS.readFileSync(Path.join(__dirname, 'index.html')));
+			const template = FS.readFileSync('./views/index.html');
+			res.end(ejs.render(template, {}));
+		}
+		break;
 		case '/submitEndpoint':
 		if(req.method === 'POST'){
 			console.log('POSTED');	
 			console.log((req.headers));
+
+			req.on('data', chunk => {
+                let writeStream = FS.createWriteStream('./files/' + (+ new Date()) );
+                writeStream.write(chunk);
+			});
 		}
 		res.writeHead(301, {'Location': '/'});
 		res.end();
 		break;
-		case '/':
 		default:
-		if(req.method === 'GET'){
-			res.writeHead(200, {'content-type': 'text/html'});
-			res.end(FS.readFileSync(Path.join(__dirname, 'index.html')));
-		}
-		break;
 	}
 });
 
